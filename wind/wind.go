854d5af4211429cdbd4578ddcdabff42681e717a
Evan.lu <evan.lu@lianwei.com.cn>
2022-03-14 17:44:04 +0800

    go 1.18beta beta0.1.3

--- wind/wind.go
+++ wind/wind.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/iEvan-lhr/nihility-dust/anything"
+	"log"
 	"reflect"
 	"sync"
 	"time"
@@ -22,10 +23,12 @@ type Wind struct {
 func (w *Wind) Schedule(startName string, inData ...any) int64 {
 	key := w.IWork.GetId()
 	go func(I int64) {
+		defer func() {
+			delete(w.C, I)
+		}()
 		w.C[I] = make(chan *anything.Mission, 10)
 		w.C[I] <- &anything.Mission{
-			Name: startName,
-			//I:       key,
+			Name:    startName,
 			Pursuit: inData,
 		}
 		for {
@@ -68,6 +71,9 @@ func (w *Wind) Init() {
 		for j := 0; j < dus.NumMethod(); j++ {
 			method := dus.Method(j)
 			if method.Name != "" && method.Name != " " {
+				if _, ok := w.M[method.Name]; ok && method.Name != "Empty" {
+					log.Println("panic:", method.Name, "已存在 请检查", client)
+				}
 				w.M[method.Name] = client.MethodByName(method.Name)
 			}
 		}
@@ -92,22 +98,14 @@ func (w *Wind) Register(a ...any) {
 	w.D = append(w.D, a...)
 }
 
-//panic: interface conversion:
-//	interface {} is func(*dust.Dust, chan *anything.Mission, []interface {}),
-//				not func(chan *anything.Mission, []interface {})
-
-//你需要先去了解下 golang中   & |  <<  >> 几个符号产生的运算意义
 const (
 	workerBits  uint8 = 10 //10bit工作机器的id，如果你发现1024台机器不够那就调大次值
 	numberBits  uint8 = 12 //12bit 工作序号，如果你发现1毫秒并发生成4096个唯一id不够请调大次值
 	workerMax   int64 = -1 ^ (-1 << workerBits)
 	numberMax   int64 = -1 ^ (-1 << numberBits)
-	timeShift   uint8 = workerBits + numberBits
-	workerShift uint8 = numberBits
-	// 如果在程序跑了一段时间修改了epoch这个值 可能会导致生成相同的ID，
-	//这个值请自行设置为你系统准备上线前的精确到毫秒级别的时间戳，因为雪花时间戳保证唯一的部分最多管69年（2的41次方），
-	//所以此值设置为你当前时间戳能够保证你的系统是从当前时间开始往后推69年
-	startTime int64 = 1525705533000
+	timeShift         = workerBits + numberBits
+	workerShift       = numberBits
+	startTime   int64 = 1525705533000
 )
 
 type Worker struct {
@@ -144,10 +142,6 @@ func (w *Worker) GetId() int64 {
 		w.number = 0
 		w.timestamp = now
 	}
-	//以下表达式才是主菜
-	//  (now-startTime)<<timeShift   产生了 41 + （10 + 12）的效应但却并不保证唯一
-	//  | (w.workerId << workerShift)  保证了与其他机器不重复
-	//  | (w.number))  保证了自己这台机不会重复
-	ID := int64((now-startTime)<<timeShift | (w.workerId << workerShift) | (w.number))
+	ID := (now-startTime)<<timeShift | (w.workerId << workerShift) | (w.number)
 	return ID
 }
